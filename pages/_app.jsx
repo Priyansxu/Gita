dad41d2ac53aece372d1935bcae6548b52cf9927
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-07-22 14:45:07 +0530

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
+function Gita({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
 
-export default MyApp;
+export default Gita;
