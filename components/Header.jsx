6a05963ee60a97ba7941c11653cba28caa36039c
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-07-22 15:40:48 +0530

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,8 +4,8 @@ import bhagwatImage from '../public/bhagwat.jpg';
 const Header = () => (
     <header className="text-center my-8">
         <Image src={bhagwatImage} alt="Bhagwat Geeta" layout="responsive" className="mx-auto"/>
-        <h1 className="font-serif text-4xl mt-4">Krishna</h1>
-        <p className="mt-4">
+        <h1 className="font-serif text-4xl mt-4 text-saffron">Krishna</h1>
+        <p className="mt-4 font-sans text-deepBlue">
             The Bhagavad Gita, often referred to as the Gita, is a 700-verse Hindu scripture that is part of the Indian epic Mahabharata.
         </p>
     </header>
