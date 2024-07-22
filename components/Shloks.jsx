3588588efc7e771bbeafcd370e929b12c26003c5
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-07-22 19:11:39 +0530

    Update Shloks.jsx

--- components/Shloks.jsx
+++ components/Shloks.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import Modal from './Modal';
 
 const VerseDetail = ({ selectedChapter, selectedVerse, onClose }) => {
     const [verseData, setVerseData] = useState(null);
@@ -28,20 +27,23 @@ const VerseDetail = ({ selectedChapter, selectedVerse, onClose }) => {
     if (!verseData) return null;
 
     return (
-        <Modal onClose={onClose}>
-            <h2 className="text-2xl font-bold mb-4 font-serif text-gold">Shlok</h2>
-            <div className="text-deepBlue">
-                <h3 className="text-xl mb-2">Chapter: {selectedChapter}, Verse: {selectedVerse}</h3>
-                <p><strong>Shlok:</strong> {verseData.shlok}</p>
-                <p><strong>Explanation:</strong> {verseData.tej}</p>
-                <p><strong>English Translation:</strong> {verseData.trans}</p>
-                <div className="mt-4">
-                    <h4 className="text-lg font-semibold">Summary:</h4>
-                    <p><strong>English:</strong> {verseData.summary.en}</p>
-                    <p><strong>Hindi:</strong> {verseData.summary.hi}</p>
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+            <div className="bg-white p-8 rounded-lg shadow-lg max-w-2xl w-full">
+                <button onClick={onClose} className="absolute top-2 right-2 text-2xl">&times;</button>
+                <h2 className="text-2xl font-bold mb-4 font-serif text-gold">Shlok</h2>
+                <div className="text-deepBlue">
+                    <h3 className="text-xl mb-2">Chapter: {selectedChapter}, Verse: {selectedVerse}</h3>
+                    <p><strong>Shlok:</strong> {verseData.shlok}</p>
+                    <p><strong>Explanation:</strong> {verseData.tej}</p>
+                    <p><strong>English Translation:</strong> {verseData.trans}</p>
+                    <div className="mt-4">
+                        <h4 className="text-lg font-semibold">Summary:</h4>
+                        <p><strong>English:</strong> {verseData.summary.en}</p>
+                        <p><strong>Hindi:</strong> {verseData.summary.hi}</p>
+                    </div>
                 </div>
             </div>
-        </Modal>
+        </div>
     );
 };
 
