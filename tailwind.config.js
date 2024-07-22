d00ab33d5309e44c205048a0d36c228c2225fae7
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-07-22 15:39:47 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        saffron: '#FF9933',
+        deepBlue: '#002366',
+        gold: '#FFD700',
+        cream: '#F5F5DC',
+        maroon: '#800020',
+      },
+      fontFamily: {
+        serif: ['Merriweather', 'serif'],
+        sans: ['Noto Serif', 'serif'],
+        alternative: ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
