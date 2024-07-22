253f4c313cbeaf982fbd6e7963ce57acf3b34936
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-07-22 23:18:18 +0530

    Create page.js

---
+++ pages/page.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import Header from '../components/Header';
+import Adhyay from '../components/Adhyay';
+import Shloks from '../components/Shloks';
+
+const Home = () => {
+    const [selectedChapter, setSelectedChapter] = useState(null);
+    const [selectedVerse, setSelectedVerse] = useState(null);
+
+    const handleShlokClick = (chapter, verse) => {
+        setSelectedChapter(chapter);
+        setSelectedVerse(verse);
+    };
+
+    const handleClose = () => {
+        setSelectedChapter(null);
+        setSelectedVerse(null);
+    };
+
+    return (
+        <div className="relative">
+            <Header />
+            <Adhyay onShlokClick={handleShlokClick} />
+            {selectedChapter !== null && selectedVerse !== null && (
+                <Shloks 
+                    selectedChapter={selectedChapter} 
+                    selectedVerse={selectedVerse} 
+                    onClose={handleClose}
+                />
+            )}
+        </div>
+    );
+};
+
+export default Home;
