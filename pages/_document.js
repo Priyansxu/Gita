96267bd6990766c0795c24d006d6f39b8e9fbdef
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-07-23 13:20:42 +0530

    Update pages

---
+++ pages/_document.js
@@ -0,0 +1,26 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="theme-color" content="#002366" />
+        <meta name="description" content="Bhagavad Gita Shloks." />
+        <meta name="author" content="Sacred verses of the Bhagavad Gita." />
+        <meta name="keywords" content="Bhagavad, Gita" />
+
+        <meta property="og:title" content="Bhagavad Gita Shlok" />
+        <meta property="og:description" content="Sacred verses of the Bhagavad Gita." />
+        <meta property="og:image" content="https://bhagavad.pages.dev/header.jpg" />
+        <meta property="og:url" content="https://bhagavad.pages.dev" />
+        <meta property="og:site_name" content="Bhagavad Gita Shloks" />
+
+        <link rel="canonical" href="https://bhagavad.pages.dev" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
