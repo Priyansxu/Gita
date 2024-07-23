0d8ee4c0cd56c41702521d431cacae23c355a835
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-07-23 13:01:19 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -2,12 +2,12 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <footer className="bg-deepBlue text-cream py-4">
+    <footer className="bg-gradient-to-r from-deepBlue via-cream to-deepBlue text-cream py-4 rounded-t-lg">
       <div className="container mx-auto text-center">
         <p className="text-lg font-alternative">
           Made with love by{' '}
           <Link href="https://www.instagram.com/pusxoo">
-            <a className="text-saffron hover:text-lightSaffron transition-colors">
+            <a className="font-bold text-saffron hover:text-lightSaffron transition-colors">
               Priyanshu 🪷
             </a>
           </Link>
