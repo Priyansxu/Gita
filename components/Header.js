da9ba81560159686dbbe71cc058b0c8c456e0ab5
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-07-23 15:14:17 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Image from 'next/image';
 import headerImage from '../public/header.jpg';
+import fluteImage from '../public/flute.png';
 
 const Header = () => {
   return (
     <header className="relative text-center my-8 px-4">
       <div className="fixed left-0 top-0 w-full max-w-5xl bg-deepBlue border-b border-deepBlue backdrop-blur-2xl z-10 mb-20">
-        <p className="text-center text-saffron text-2xl lg:text-2xl font-serif py-2">
+        <p className="text-center text-saffron text-2xl lg:text-2xl font-serif py-2 flex items-center justify-center">
           Bhagavad&nbsp;Gita
+          <Image 
+            src={fluteImage} 
+            alt="Flute" 
+            width={30} 
+            height={30} 
+            className="ml-2 object-contain"
+          />
         </p>
       </div>
       <div className="relative mx-auto my-7 p-3 shadow-lg">
