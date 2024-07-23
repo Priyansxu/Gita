c3f0f9ce4ebda0c1387162ebd5cdcdcaf3343b81
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-07-23 15:20:49 +0530

    Update Shlok.js

--- components/Shlok.js
+++ components/Shlok.js
@@ -12,7 +12,6 @@ const Shlok = ({ selectedChapter, selectedVerse, onClose }) => {
                     const geeta = response.data;
                setVerseData({
                   shlok: geeta.slok,
-                  tran: geeta.transliteration,
                   hindi: geeta.tej.ht,
                   english: geeta.siva.et
                     });
@@ -43,7 +42,6 @@ const Shlok = ({ selectedChapter, selectedVerse, onClose }) => {
                 <div className="text-deepBlue">
                     <h3 className="text-lg md:text-xl mb-2 text-center">Chapter: {selectedChapter}, Verse: {selectedVerse}</h3>
                     <p className="my-2 text-sm md:text-base"><strong>Shlok:</strong> {verseData.shlok}</p>
-                    <p className="my-2 text-sm md:text-base"><strong>Transliteration:</strong> {verseData.tran}</p>
                     <p className="my-2 text-sm md:text-base"><strong>Explanation:</strong> {verseData.hindi}</p>
                     <p className="my-2 text-sm md:text-base"><strong>English Translation:</strong> {verseData.english}</p>
                     <p className="my-2 text-sm md:text-base"><strong>Summary:</strong> {verseSummary}</p>
