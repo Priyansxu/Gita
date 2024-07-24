416ab5947b6c3b27007bf59732ad08aa8525df35
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-07-24 14:43:04 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
    <div className="px-4">
     <footer className="bg-deepBlue text-cream py-4 rounded-t-lg">
       <div className="container mx-auto text-center">
-        <p className="text-sm font-serif  underline decoration-gold">
+        <p className="text-sm font-serif">
           Made with love by{' '}
           <Link href="https://www.instagram.com/pusxoo">
             <a className="font-serif text-saffron hover:text-lightSaffron transition-colors">
