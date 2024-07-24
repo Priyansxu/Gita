6331384032d783c01bde612239b334c9bcfb77c3
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-07-24 13:51:21 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <footer className="bg-deepBlue text-cream py-4 rounded-t-lg">
+    <footer className="bg-deepBlue text-cream py-4 px-4 rounded-t-lg">
       <div className="container mx-auto text-center">
         <p className="text-base font-serif">
           Made with love by{' '}
